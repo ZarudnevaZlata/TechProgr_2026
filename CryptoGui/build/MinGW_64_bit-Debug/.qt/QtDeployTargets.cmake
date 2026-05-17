64ca45d52b6f1bf031babd0e68cac2108ea55095
ZarudnevaZlata <zarudnevazlata2017@gmail.com>
2026-05-17 19:42:21 +0300

    Удалены временные папки сборки

--- CryptoGui/build/MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CryptoGui_FILE C:/Users/zarud/OneDrive/Desktop/prog/CryptoGui/build/MinGW_64_bit-Debug/CryptoGui.exe)
-set(__QT_DEPLOY_TARGET_CryptoGui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CryptoGui_RUNTIME_DLLS D:/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/6.11.0/mingw_64/bin/Qt6Network.dll;D:/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/6.11.0/mingw_64/bin/Qt6Core.dll)
--- CryptoGui/build/MinGW_64_bit-Debug/.qt/deploy_CryptoGui_8ade4a5cbd.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/zarud/OneDrive/Desktop/prog/CryptoGui/build/MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/CryptoGui-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/zarud/OneDrive/Desktop/prog/CryptoGui/build/MinGW_64_bit-Debug/CryptoGui.exe"
-    GENERATE_QT_CONF
-)
