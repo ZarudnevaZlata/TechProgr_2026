1adf01de407095c480daf2550037853a4a58327a
kiliyli <jjuli.kalininaa@icloud.com>
2026-05-27 00:42:33 +0300

    Update AlgorithmService.h
    vigenere

--- Visual Studio/CryptoServer/AlgorithmService.h
+++ Visual Studio/CryptoServer/AlgorithmService.h
@@ -6,8 +6,8 @@ using namespace std;
 
 class AlgorithmService {
 public:
-    string vigenereEncryptStub();
-    string vigenereDecryptStub();
+    string vigenereEncrypt(const string& text, const string& key);
+    string vigenereDecrypt(const string& text, const string& key);
     string sha1Stub();
     string newtonStub();
     string audioEmbedStub();
